e5a3c35aa9cae9cdc7af41d2a2eec2a5ef2dfd57
psk <preetskhalsa97@gmail.com>
2017-01-26 20:01:42 +0530

    final?

--- Visualise.R
+++ Visualise.R
@@ -23,8 +23,7 @@ print (plot)
 dev.off()
 
 """
-Coloring points by region and plotting a regression line
-"""
+Coloring points by region and plotting a regression line"""
 ggplot(who,aes(x=GNI,y=FertilityRate,color=Region))+geom_point() +xlab("hfyhtgrfe") #x axis label
 #adding regression line to the plot 
 ggplot(who,aes(x=GNI,y=FertilityRate,color=Region))+geom_point() + stat_smooth(method=lm)
@@ -44,4 +43,91 @@ WeekdayCounts$Var1 = factor(WeekdayCounts$Var1, ordered=TRUE, levels=c("Sunday",
 
 # Try again:
 ggplot(WeekdayCounts, aes(x=Var1, y=Freq)) + geom_line(aes(group=1))
+
+#Making Heat Maps
+DayHourCounts = as.data.frame(table(mvt$Weekday, mvt$Hour))
+
+DayHourCounts$Hour = as.numeric(as.character(DayHourCounts$Var2))
+
+ggplot(DayHourCounts, aes(x=Hour, y=Freq)) + geom_line(aes(group=Var1)) #Var1-> day of the week ==> group by days==> we get 7 lines
+ggplot(DayHourCounts, aes(x=Hour, y=Freq)) + geom_line(aes(group=Var1,color=Var1)) #diff colors for diff days 
+
+# Make a heatmap:
+ggplot(DayHourCounts, aes(x = Hour, y = Var1)) + geom_tile(aes(fill = Freq))
+
+# Change the color scheme
+ggplot(DayHourCounts, aes(x = Hour, y = Var1)) + geom_tile(aes(fill = Freq)) + scale_fill_gradient(name="Total MV Thefts", low="white", high="red") + theme(axis.title.y = element_blank())
+
+"""
+Geographical Maps
+"""
+# Install and load two new packages:
+install.packages("maps")
+install.packages("ggmap")
+library(maps)
+library(ggmap)
+
+# Load a map of Chicago into R:
+chicago = get_map(location = "chicago", zoom = 11)
+
+# Look at the map
+ggmap(chicago)
+
+# Plot the first 100 motor vehicle thefts:
+ggmap(chicago) + geom_point(data = mvt[1:100,], aes(x = Longitude, y = Latitude))
+
+# Round our latitude and longitude to 2 digits of accuracy, and create a crime counts data frame for each area:
+LatLonCounts = as.data.frame(table(round(mvt$Longitude,2), round(mvt$Latitude,2)))
+
+str(LatLonCounts)
+
+# Convert our Longitude and Latitude variable to numbers:
+LatLonCounts$Long = as.numeric(as.character(LatLonCounts$Var1))
+LatLonCounts$Lat = as.numeric(as.character(LatLonCounts$Var2))
+
+# Plot these points on our map:
+ggmap(chicago) + geom_point(data = LatLonCounts, aes(x = Long, y = Lat, color = Freq, size=Freq))
+
+# Change the color scheme:
+ggmap(chicago) + geom_point(data = LatLonCounts, aes(x = Long, y = Lat, color = Freq, size=Freq)) + scale_colour_gradient(low="yellow", high="red")
+
+# We can also use the geom_tile geometry
+ggmap(chicago) + geom_tile(data = LatLonCounts, aes(x = Long, y = Lat, alpha = Freq), fill="red")
+
+#--------------------------------------------------------------#
+murders = read.csv("murders.csv")
+
+str(murders)
+
+# Load the map of the US
+statesMap = map_data("state")
+
+str(statesMap)
+
+# Plot the map:
+ggplot(statesMap, aes(x = long, y = lat, group = group)) + geom_polygon(fill = "white", color = "black") 
+
+# Create a new variable called region with the lowercase names to match the statesMap:
+murders$region = tolower(murders$State)
+
+# Join the statesMap data and the murders data into one dataframe:
+murderMap = merge(statesMap, murders, by="region")
+str(murderMap)
+
+# Plot the number of murder on our map of the United States:
+ggplot(murderMap, aes(x = long, y = lat, group = group, fill = Murders)) + geom_polygon(color = "black") + scale_fill_gradient(low = "black", high = "red", guide = "legend")
+
+# Plot a map of the population:
+ggplot(murderMap, aes(x = long, y = lat, group = group, fill = Population)) + geom_polygon(color = "black") + scale_fill_gradient(low = "black", high = "red", guide = "legend")
+
+# Create a new variable that is the number of murders per 100,000 population:
+murderMap$MurderRate = murderMap$Murders / murderMap$Population * 100000
+
+# Redo our plot with murder rate:
+ggplot(murderMap, aes(x = long, y = lat, group = group, fill = MurderRate)) + geom_polygon(color = "black") + scale_fill_gradient(low = "black", high = "red", guide = "legend")
+
+# Redo the plot, removing any states with murder rates above 10:
+ggplot(murderMap, aes(x = long, y = lat, group = group, fill = MurderRate)) + geom_polygon(color = "black") + scale_fill_gradient(low = "black", high = "red", guide = "legend", limits = c(0,10))
+
+
 
